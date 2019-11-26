f353543749dc75574717b28373b35edfdf692bba
Mike <michael005@me.com>
2019-11-26 13:10:50 -0500

    Displayed Search w/API

---
+++ MovieAPIProject/MovieAPIProject/Controllers/MovieController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieAPIProject.Models;
+
+namespace MovieAPIProject.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly HttpClient _client;
+
+        public MovieController(IHttpClientFactory client)
+        {
+            _client = client.CreateClient();
+            _client.BaseAddress = new Uri("http://www.omdbapi.com/");
+        }
+        public async Task<IActionResult> DisplaySearch()
+        {
+            var response = await _client.GetAsync("?s=ghostbusters&apikey=425dbd7b");
+            var movies = await response.Content.ReadAsAsync<List<MovieRoot>>();
+            return View(movies);
+    }
+}
