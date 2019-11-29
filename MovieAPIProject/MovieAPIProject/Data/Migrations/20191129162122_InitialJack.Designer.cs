53d1d6f70ed4e3a8756c0e3e835af24a77e530a9
Johnathon Lubaway <NeoJax@gmail.com>
2019-11-29 12:04:08 -0500

    Added validation sorta for the forms and if a user is logged in or not

--- MovieAPIProject/MovieAPIProject/Data/Migrations/20191129162122_InitialJack.Designer.cs
+++ MovieAPIProject/MovieAPIProject/Data/Migrations/20191129162122_InitialJack.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieAPIProject.Data;
 
 namespace MovieAPIProject.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191129162122_InitialJack")]
+    partial class InitialJack
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
