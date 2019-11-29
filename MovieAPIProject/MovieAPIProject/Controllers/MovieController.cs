b95943e9562b01b6bbf2e692f288167788ce2498
Mike <michael005@me.com>
2019-11-29 10:51:33 -0500

    Added Search by Year Method + View and new Form on HomePage

--- MovieAPIProject/MovieAPIProject/Controllers/MovieController.cs
+++ MovieAPIProject/MovieAPIProject/Controllers/MovieController.cs
@@ -29,7 +29,7 @@ namespace MovieAPIProject.Controllers
         public async Task<IActionResult> DisplayByYear(string year)
         {
             var response = await _client.GetAsync($"?y={year}&apikey=425dbd7b");
-            var movies = await response.Content.ReadAsAsync<List<SearchRoot>>();
+            var movies = await response.Content.ReadAsAsync<SearchRoot>();
 
             return View(movies);
         }
